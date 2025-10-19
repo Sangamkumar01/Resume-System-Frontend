232950e75d95eaeae0f8c2c31322f9c424659f8b
SANGAM KUMAR <103239546+Sangamkumar01@users.noreply.github.com>
2025-10-19 11:48:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  server: {
+    host: "0.0.0.0", // exposes server to external requests
+    port: process.env.PORT || 5173, // Render sets PORT dynamically
+    allowedHosts: ["resume-system-frontend.onrender.com"], // ✅ your Render frontend domain
   },
-})
+});
