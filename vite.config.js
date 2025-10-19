be392661a5c967c5977a233dfd16f8d14cd8f1f7
SANGAM KUMAR <103239546+Sangamkumar01@users.noreply.github.com>
2025-10-19 11:52:37 +0530

    Add path alias resolution to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // exposes server to external requests
-    port: process.env.PORT || 5173, // Render sets PORT dynamically
-    allowedHosts: ["resume-system-frontend.onrender.com"], // ✅ your Render frontend domain
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-});
+})
